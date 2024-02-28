a0936fdb7c3e8f34d67a6fc913dfb1317b325981
taio1000 <tadeoelli@hotmail.com>
2024-02-28 20:21:03 -0300

    Revert "comit"
    This reverts commit 911edcf7c96f5dec2f881a6c3106eaf3fc67c745.

--- MMM Project/Assets/Scripts/Towers/Strategys/AccelerationTowerBehaviour.cs
+++ MMM Project/Assets/Scripts/Towers/Strategys/AccelerationTowerBehaviour.cs
@@ -9,7 +9,7 @@ public class AccelerationTowerBehaviour : TowerStrategy
     [SerializeField] private float attractionStrength;
     [SerializeField] private float repulsionRadius;
     [SerializeField] private float radius;
-    [SerializeField] private float maxRotationSpeed;
+
     [SerializeField] private float repulsionStrength;
     [SerializeField] private float angularAcceleration;
 
@@ -18,61 +18,51 @@ public class AccelerationTowerBehaviour : TowerStrategy
         tower.transform.position = origin;
     }
     public override void SpecialBehaviour(GameObject prefab, GameObject other){
-        if (other.CompareTag("Missiles"))
-        {
+        if(other.CompareTag("Missiles")){
             Rigidbody2D projectileRigidbody = other.GetComponent<Rigidbody2D>();
             MissileBehavior missile = other.GetComponent<MissileBehavior>();
 
             if (projectileRigidbody != null)
             {
-                Vector2 localPosition = prefab.transform.InverseTransformPoint(other.transform.position);
-                float distance = localPosition.magnitude;
+                Vector2 direction = prefab.transform.position - other.transform.position;
+                float distance = direction.magnitude;
 
-                if (distance < radius && !missile.hasBeenAttracted)
+                if (distance < radius && !missile.hasBeenAtracted)
                 {
-                    if (distance > repulsionRadius)
-                    {
+                    if(distance > repulsionRadius && !missile.hasBeenAtracted){
                         // Fuerza de atracción proporcional a la distancia
                         float attractionForce = attractionStrength / distance;
 
                         // Aplicar fuerza de atracción
-                        projectileRigidbody.AddForce(localPosition.normalized * attractionForce, ForceMode2D.Force);
+                        projectileRigidbody.AddForce(direction.normalized * attractionForce, ForceMode2D.Force);
                     }
-                    else
-                    {
-                        // Detener el proyectil cuando está dentro del radio de repulsión
+                    else if(distance < repulsionRadius && !missile.hasBeenAtracted){
                         projectileRigidbody.velocity = Vector2.zero;
-                        missile.hasBeenAttracted = true;
+                        missile.hasBeenAtracted = true;
                     }
+                    else{
+                        // Fuerza de repulsión proporcional a la distancia
+                        float repulsionForce = repulsionStrength / distance;
 
-                    // Determinar el sentido de rotación (horario o antihorario) según la posición local
-                    float rotationDirection = (localPosition.x > 0) ? 1f : -1f;
+                        // Aplicar fuerza de repulsión
+                        projectileRigidbody.AddForce(direction.normalized * repulsionForce, ForceMode2D.Force);
+                        // Rotación gradual alrededor de la torre
+                        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                        other.transform.RotateAround(prefab.transform.position, Vector3.forward, missile.rotationSpeed * Time.deltaTime);
 
-                    // Rotación gradual alrededor de la torre
-                    other.transform.RotateAround(prefab.transform.position, Vector3.forward, rotationDirection * missile.rotationSpeed * Time.deltaTime);
-
-                    // Aumentar velocidad angular con el tiempo
-                    missile.rotationSpeed = Mathf.Clamp(missile.rotationSpeed + angularAcceleration * Time.deltaTime, 0f, maxRotationSpeed);
-                }
-                else
-                {
-                    // El proyectil está fuera del rango de atracción
-                    if (missile.hasBeenAttracted)
-                    {
-                        // Puedes reiniciar el estado del proyectil para que pueda ser atraído nuevamente
-                        missile.hasBeenAttracted = false;
-                        missile.rotationSpeed = 0f;
+                        // Aumentar velocidad angular con el tiempo
+                        missile.rotationSpeed = missile.rotationSpeed + angularAcceleration * Time.deltaTime;
                     }
-
-                    // Fuerza de repulsión proporcional a la distancia
-                    float repulsionForce = repulsionStrength / distance;
-
-                    // Aplicar fuerza de repulsión en la dirección opuesta a la posición local
-                    projectileRigidbody.AddForce(-localPosition.normalized * repulsionForce, ForceMode2D.Force);
                 }
+                else if(distance > radius && missile.hasBeenAtracted){
+                    missile.hasBeenAtracted = false;
+                    //missile.rotationSpeed = 100f;
+                    projectileRigidbody.AddForce(direction.normalized * (repulsionStrength * 100), ForceMode2D.Force);
+                }
+                
             }
+
         }
-    
     }
 
     public override bool ColliderBehaviour(GameObject prefab, GameObject other){
