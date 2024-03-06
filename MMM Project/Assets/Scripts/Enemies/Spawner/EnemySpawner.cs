4f81f3f3e31cbc44deca11088ddf3353b8bc4dfd
taio1000 <tadeoelli@hotmail.com>
2024-03-06 10:48:03 -0300

    EnemySpawner

--- MMM Project/Assets/Scripts/Enemies/Spawner/EnemySpawner.cs
+++ MMM Project/Assets/Scripts/Enemies/Spawner/EnemySpawner.cs
@@ -8,64 +8,99 @@ public class EnemySpawner : MonoBehaviour
     public int _currentWaveCount;       //The index of the current wave [Remember, a list starts from 0]
 
     [Header("Spawner Attributes")]
-    float _spawnTimer; //timer use to determine whe to spawn the next enemy
+    float _spawnTimerForEnemies; //timer use to determine whe to spawn the next enemy
+
+    float _spawnTimerForGroup; //timer use to determine whe to spawn the next group
     public int _enemiesAlive;
     public int _maxEnemiesAllowed;      //The maximum number of enemies allowed on tthe map at once
     public bool _maxEnemiesReached = false;     //a flag indicatin if the maximum number of enemies has been reached
+    private bool hasToSpawnAGroup = true;
 
 
-    [Header("Spawn Positions")]
+    [Header("Spawn Positions For Enemies")]
     public List<Transform> _basicSpawnPoints;        //A lst to store all the relative spawn points of enemies
+
+    [Header("Spawn Positions For Group of Enemies")]
+    public List<GameObject> _prefabSpawnPoints;        //A lst to store all the relative spawn points of enemies
 
     [SerializeField] private EnemyPool _pool;
     void Start() 
     {
         CalculateWaveQuota();
+        SpawnGroupOfEnemies();
     }
     void Update() 
     {
 
-        _spawnTimer += Time.deltaTime;
+        _spawnTimerForEnemies += Time.deltaTime;
+        _spawnTimerForGroup += Time.deltaTime;
 
+        if(_spawnTimerForGroup >= _waves[_currentWaveCount]._spawnIntervalForGroup){
+            hasToSpawnAGroup = true;
+            _spawnTimerForGroup = 0f;
+            SpawnGroupOfEnemies();
+        }
         //Check if it´s time to spawn the next enemy
-        if(_spawnTimer >= _waves[_currentWaveCount]._spawnInterval)
+        if(_spawnTimerForEnemies >= _waves[_currentWaveCount]._spawnIntervalForEnemy && !hasToSpawnAGroup)
         {
-            _spawnTimer = 0f;
+            _spawnTimerForEnemies = 0f;
             SpawnEnemies();
             
         }
     }
 
+    void SpawnGroupOfEnemies()
+    {
+        int groupIndex = Random.Range(0,_prefabSpawnPoints.Count);
+        List<Transform> spawnPoints = new List<Transform>();
+        _prefabSpawnPoints[groupIndex].GetComponentsInChildren<Transform>(false, spawnPoints);
+        //Check if the minimum number of enemies in the wave have been spawned
+        if(_waves[_currentWaveCount]._spawnGroupCount < _waves[_currentWaveCount]._waveGroupQuota)
+        {
+            //Spawn each type of enemy until the quota is filled
+            foreach (var spawn in spawnPoints)
+            {
+                int index = Random.Range(0,_waves[_currentWaveCount]._enemyGroups.Count);
+                GameObject enemy = _pool.RequestEnemy(_waves[_currentWaveCount]._enemyGroups[index].enemyPrefab);
+                enemy.GetComponent<EnemyBehaviour>().normalDir =  _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection;
+                enemy.transform.rotation =  _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection ? Quaternion.Euler(0f, 0f, 90): Quaternion.Euler(0f, 0f, 270);
+                Vector3 offset = _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection ? new Vector3 (12,0,0): new Vector3(-12,0,0);
+                enemy.transform.position = offset + spawn.position;              
+            }
+            _waves[_currentWaveCount]._spawnGroupCount++;
+            hasToSpawnAGroup = false;
+            _spawnTimerForEnemies = 0f;
+            _enemiesAlive++;
+        }
+    }
     void SpawnEnemies()
     {
         //Check if the minimum number of enemies in the wave have been spawned
-        if(_waves[_currentWaveCount]._spawnCount < _waves[_currentWaveCount]._waveQuota && !_maxEnemiesReached)
+        if(_waves[_currentWaveCount]._spawnEnemyCount < _waves[_currentWaveCount]._waveEnemyQuota && !_maxEnemiesReached)
         {
             //Spawn each type of enemy until the quota is filled
-            foreach (var enemyGroup in _waves[_currentWaveCount]._enemyGroups)
+            int index = Random.Range(0,_waves[_currentWaveCount]._enemyGroups.Count);
+            //Check if the minimum number of enemies of this type have been spwawned
+            if(_waves[_currentWaveCount]._enemyGroups[index]._spawnCount < _waves[_currentWaveCount]._enemyGroups[index]._enemyCount)
             {
-                //Check if the minimum number of enemies of this type have been spwawned
-                if(enemyGroup._spawnCount < enemyGroup._enemyCount)
+                if(_enemiesAlive >= _maxEnemiesAllowed)
                 {
-                    if(_enemiesAlive >= _maxEnemiesAllowed)
-                    {
-                        _maxEnemiesReached = true;
-                        return;
-                    }
-                    
-                    //Spawn the enemy at a random position close tothe player
-                    GameObject enemy = _pool.RequestEnemy(enemyGroup.enemyPrefab);
-                    enemy.GetComponent<EnemyBehaviour>().normalDir =  enemyGroup._enemyDirection;
-                    enemy.transform.rotation =  enemyGroup._enemyDirection ? Quaternion.Euler(0f, 0f, 90): Quaternion.Euler(0f, 0f, 270);
-                    Vector3 offset = enemyGroup._enemyDirection ? new Vector3 (11,0,0): new Vector3(-11,0,0);
-                    enemy.transform.position = offset + _basicSpawnPoints[Random.Range(0,_basicSpawnPoints.Count)].position;
-                    
-                    //enemy.transform.parent = transform;
-
-                    enemyGroup._spawnCount++;
-                    _waves[_currentWaveCount]._spawnCount++;
-                    _enemiesAlive++;
+                    _maxEnemiesReached = true;
+                    return;
                 }
+                
+                //Spawn the enemy at a random position close tothe player
+                GameObject enemy = _pool.RequestEnemy(_waves[_currentWaveCount]._enemyGroups[index].enemyPrefab);
+                enemy.GetComponent<EnemyBehaviour>().normalDir =  _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection;
+                enemy.transform.rotation =  _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection ? Quaternion.Euler(0f, 0f, 90): Quaternion.Euler(0f, 0f, 270);
+                Vector3 offset = _waves[_currentWaveCount]._enemyGroups[index]._enemyDirection ? new Vector3 (14,0,0): new Vector3(-14,0,0);
+                enemy.transform.position = offset + _basicSpawnPoints[Random.Range(0,_basicSpawnPoints.Count)].position;
+                
+                //enemy.transform.parent = transform;
+
+                _waves[_currentWaveCount]._enemyGroups[index]._spawnCount++;
+                _waves[_currentWaveCount]._spawnEnemyCount++;
+                _enemiesAlive++;
             }
         }
         //Reset the maxEnemiesReached flag if the number of enemies alive has dropped below the maximum amount
@@ -83,7 +118,7 @@ public class EnemySpawner : MonoBehaviour
             _currentWaveQuota += enemyGroup._enemyCount;
         }
 
-        _waves[_currentWaveCount]._waveQuota = _currentWaveQuota;
+        _waves[_currentWaveCount]._waveEnemyQuota = _currentWaveQuota;
         Debug.Log("Cantidad de enemigos en esta oleada:" + _currentWaveQuota);
     }
 }
--- MMM Project/Assets/Scripts/Enemies/Spawner/Wave.cs
+++ MMM Project/Assets/Scripts/Enemies/Spawner/Wave.cs
@@ -7,8 +7,11 @@ public class Wave
 {
     public string _waveName;
     public List<EnemyGroup> _enemyGroups;   //A list of grups of enemies to spawn in this wave
-    public int _waveQuota;      //The total number of enemies to spawn in this wave
-    public float _spawnInterval;    //The interval at which to spawn enemies
-    public int _spawnCount;     //The number of enemies already spawned in this wave
+    public int _waveEnemyQuota;      //The total number of enemies to spawn in this wave
+    public int _waveGroupQuota;
+    public float _spawnIntervalForEnemy;    //The interval at which to spawn enemies random
+    public float _spawnIntervalForGroup;        //The interval at wich spawn a list of enemies in group
+    public int _spawnEnemyCount;     //The number of enemies already spawned in this wave
+    public int _spawnGroupCount;
 
 }
