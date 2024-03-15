63c5d39a57f4ec02076b13ffe4949c8ecceefd0a
taio1000 <tadeoelli@hotmail.com>
2024-03-15 17:13:41 -0300

    BasicExplosion

--- MMM Project/Assets/Scripts/Explosions/Strategy/BasicExplosionStrategy.cs
+++ MMM Project/Assets/Scripts/Explosions/Strategy/BasicExplosionStrategy.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Explosion", menuName = "ScriptableObject/Explosion/Basic", order = 0)]
 public class BasicExplosionStrategy : ExplosionStrategy
 {
+    [SerializeField] private float force;
     public override void DealDamage(Transform origin){
         Collider2D[] objetos = Physics2D.OverlapCircleAll(origin.position, radius);
 
@@ -13,6 +14,17 @@ public class BasicExplosionStrategy : ExplosionStrategy
 
             if(collisions.CompareTag("Nexus")){
                 collisions.GetComponent<NexusCollisions>().TakeDamageForMissile(explosionType);
+            }
+            if(collisions.CompareTag("Enemy")){
+
+                collisions.GetComponent<EnemyBehaviour>().TakeDamageForExplosion(explosionType);
+                Rigidbody2D rb2D = collisions.GetComponent<Rigidbody2D>();
+                if(rb2D != null){
+                    Vector2 direction = collisions.transform.position - origin.position;
+                    float distance = 1 + direction.magnitude;
+                    float finalForce = force / distance;
+                    rb2D.AddForce(direction * finalForce);
+                }
             }
         }
     }
