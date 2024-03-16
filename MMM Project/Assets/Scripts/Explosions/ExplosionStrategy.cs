bfdf36f495fb4280515db343edfce8462a3e2c80
taio1000 <tadeoelli@hotmail.com>
2024-03-16 19:54:04 -0300

    MissilesExplosion

--- MMM Project/Assets/Scripts/Explosions/ExplosionStrategy.cs
+++ MMM Project/Assets/Scripts/Explosions/ExplosionStrategy.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 
 public abstract class ExplosionStrategy : ScriptableObject       
 {
-    public int id;
     public float radius;
-    public float damage;
-    public GameObject prefab;
     public ExplosionsTypes explosionType;
-    public GameObject CreateMissile(Transform origin){
-        GameObject explosion = ExplosionPool.Instance.RequestExplosion(prefab);
-        explosion.transform.position = origin.position;
-        return  explosion;
-    } 
+
     public abstract void DealDamage(Transform origin);
     public abstract void ExplosionBehaviour(Transform origin);
     public abstract void ImplosionBehaviour(Transform origin);
