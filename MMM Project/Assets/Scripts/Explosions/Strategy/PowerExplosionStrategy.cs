c989b24b7b79a097e8ed718f2d29c536bf967c64
taio1000 <tadeoelli@hotmail.com>
2024-07-08 15:43:25 -0300

    retoreScript

--- MMM Project/Assets/Scripts/Explosions/Strategy/PowerExplosionStrategy.cs
+++ MMM Project/Assets/Scripts/Explosions/Strategy/PowerExplosionStrategy.cs
@@ -6,56 +6,48 @@ using System.Linq;
 [CreateAssetMenu(fileName = "New Explosion", menuName = "ScriptableObject/Explosion/Power", order = 1)]
 public class PowerExplosionStrategy : ExplosionStrategy
 ///Este tipo de explosiones solo afectan a los enemigos
-{//IA2-P2”.
+{
     [SerializeField] private float implosionForce, explosionForce;
-    public override void DealDamage(Transform origin){  //Por cada collider dentro del radio, si es enemigo llama a la funcion de hacer daño
-    //y le manda el tipo de explosion
-        var gridEntities = Query(origin).ToList();
-        var gridEnemy = gridEntities.OfType<EnemyBehaviour>().Where(x => x.col.enabled == true).ToList();
+    public override void DealDamage(Transform origin)
+    {  //Por cada collider dentro del radio, si es enemigo llama a la funcion de hacer daño
+       //y le manda el tipo de explosion
+        Collider2D[] objects = Physics2D.OverlapCircleAll(origin.position, radius).Where(collision => collision.CompareTag("Enemy")).ToArray();
 
-        foreach (var collisions in gridEnemy){
-            collisions.TakeDamageForExplosion(explosionType);
+        foreach (Collider2D collisions in objects)
+        {
+            collisions.GetComponent<EnemyBehaviour>().TakeDamageForExplosion(explosionType);
         }
     }
-    public override void ExplosionBehaviour(Transform origin){  //Por cada collider dentro del radio, si es enemigo lo empuja
-    //IA2-LINQ
-    //IA2-P1"
-    //Toma todos los objetos dentro de un radio y guardo solo los de tipo rigidbody que tengan el tag correspondiente
-        var enemyBehaviours = Query(origin).OfType<EnemyBehaviour>().Where(x => x.col.enabled == true).ToList();
-
-        // Usar Aggregate para reducir objects a una lista de Rigidbody2D
-        var rigidbodies = enemyBehaviours.Aggregate(new List<Rigidbody2D>(), (list, enemy) =>
+    public override void ExplosionBehaviour(Transform origin)
+    {  //Por cada collider dentro del radio, si es enemigo lo empuja
+        Collider2D[] objects = Physics2D.OverlapCircleAll(origin.position, radius).Where(collision => collision.CompareTag("Enemy")).ToArray();
+        foreach (Collider2D collisions in objects)
+        {
+            Rigidbody2D rb2D = collisions.GetComponent<Rigidbody2D>();
+            if (rb2D != null)
             {
-                var rb = enemy.gameObject.GetComponent<Rigidbody2D>();
-                if (rb != null)
-                {
-                    Vector2 direction = rb.transform.position - origin.position;
-                    float distance = 1 + direction.magnitude;
-                    float finalForce = explosionForce / distance;
-                    rb.AddForce(direction * finalForce);
-                }
-                return list;
-            });
+                Vector2 direction = collisions.transform.position - origin.position;
+                float distance = 1 + direction.magnitude;
+                float finalForce = explosionForce / distance;
+                rb2D.AddForce(direction * finalForce);
+            }
+        }
     }
 
-    public override void ImplosionBehaviour(Transform origin){  //Por cada collider dentro del radio, si es enemigo lo atrae
-    //IA2-LINQ
-    //IA2-P1"
-    //Toma todos los objetos dentro de un radio y guardo solo los de tipo rigidbody que tengan el tag correspondiente
-        var enemyBehaviours = Query(origin).OfType<EnemyBehaviour>().Where(x => x.col.enabled == true).ToList();
-        // Usar Aggregate para reducir objects a una lista de Rigidbody2D
-        var rigidbodies = enemyBehaviours.Aggregate(new List<Rigidbody2D>(), (list, enemy) =>
+    public override void ImplosionBehaviour(Transform origin)
+    {  //Por cada collider dentro del radio, si es enemigo lo atrae
+        Collider2D[] objects = Physics2D.OverlapCircleAll(origin.position, radius).Where(collision => collision.CompareTag("Enemy")).ToArray();
+        foreach (Collider2D collisions in objects)
+        {
+            Rigidbody2D rb2D = collisions.GetComponent<Rigidbody2D>();
+            if (rb2D != null)
             {
-                var rb = enemy.gameObject.GetComponent<Rigidbody2D>();
-                if (rb != null)
-                {
-                    Vector2 direction = rb.transform.position - origin.position;
-                    float distance = 1 + direction.magnitude;
-                    float finalForce = implosionForce / distance;
-                    rb.AddForce(-direction * finalForce);
-                }
-                return list;
-            });
+                Vector2 direction = collisions.transform.position - origin.position;
+                float distance = 1 + direction.magnitude;
+                float finalForce = implosionForce / distance;
+                rb2D.AddForce(-direction * finalForce);
+            }
+        }
     }
 
 }
